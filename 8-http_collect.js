dacb3e8f03df2e4f0f4d3ac712e41b95ef9ec6b0
Dennis Liaw <dennis.liaw@gmail.com>
2015-06-23 18:56:33 -0400

    adds solution to module 8

---
+++ 8-http_collect.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var bl   = require('bl');
+
+http.get( process.argv[2], function(response){
+
+  response.pipe( bl( function(err, data){
+    if(err){
+      throw(err);
+    }
+    words = data.toString();
+
+    console.log( words.length );
+    console.log( words );
+
+    })
+  );
+
+});
